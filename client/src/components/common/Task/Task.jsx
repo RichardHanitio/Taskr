c55a48797b48e9c650a3d294e742b453135ca1a1
Richard Hanitio <richardhan82@gmail.com>
2022-08-17 14:15:17 +0700

    changed date format

--- client/src/components/common/Task/Task.jsx
+++ client/src/components/common/Task/Task.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 import "./task.scss";
-import {GoPrimitiveDot} from "react-icons/go"
-import {BiEdit} from "react-icons/bi";
-import {MdDeleteOutline} from "react-icons/md";
+import { GoPrimitiveDot } from "react-icons/go";
+import { BiEdit } from "react-icons/bi";
+import { MdDeleteOutline } from "react-icons/md";
 
-const Task = ({task, id, description, dueDate, priority, deleteTask}) => {
+const Task = ({ task, id, description, dueDate, priority, deleteTask }) => {
   let color;
   if (priority === "high") color = "danger";
   else if (priority === "medium") color = "warning";
   else color = "success";
 
-  return (
+  const day = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
 
+  const month = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+  };
+
+  const date = new Date(dueDate);
+  const now = new Date();
+  const formattedDate = `${ date.getDate()!=now.getDate() ? day[date.getDay()] : "Today"}, ${date.getDate()} ${
+    month[date.getMonth()]
+  } ${date.getFullYear()} (${date.getHours()}:${date.getMinutes()}:${date.getSeconds()})`;
+
+  return (
     <div className="task">
       <div className="task-container">
         <div className="task-priority">
@@ -19,7 +49,7 @@ const Task = ({task, id, description, dueDate, priority, deleteTask}) => {
         </div>
         <div className="task-name-due">
           <div className="task-name">{task}</div>
-          <div className="task-due">{dueDate}</div>
+          <div className="task-due">{formattedDate}</div>
         </div>
         <div className="task-edit-delete">
           <BiEdit className="task-btn task-edit-btn" />
@@ -31,6 +61,6 @@ const Task = ({task, id, description, dueDate, priority, deleteTask}) => {
       </div>
     </div>
   );
-}
+};
 
-export default Task
+export default Task;

a916309e5c15400a651c83310073562e306b1154
Richard Hanitio <richardhan82@gmail.com>
2022-09-02 16:19:07 +0700

    hello

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ const http = require("http");
 const app = require("./app");
 const connectDB = require("./db");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const myServer = http.createServer(app);
 
 const startServer = async() => {
-  await connectDB(process.env.MONGODB_URL).then(resp => console.log(resp))
-  .catch(err => console.log(err))
+  await connectDB(
+    "mongodb+srv://richardhan:JMc1LqHR6mf3FfDC@taskr-tasktracker-clust.genyqxo.mongodb.net/taskr?retryWrites=true&w=majority"
+  )
+    .then((resp) => console.log(resp))
+    .catch((err) => console.log(err));
 
   myServer.listen(port, () => {
     console.log(`Listening on port ${port}...`);

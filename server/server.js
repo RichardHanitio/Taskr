e54754a55be7f98474f610c51fd5823031ef1e31
Richard Hanitio <richardhan82@gmail.com>
2022-09-02 12:03:55 +0700

    fixed error

--- server/server.js
+++ server/server.js
@@ -4,14 +4,16 @@ const http = require("http");
 const app = require("./app");
 const connectDB = require("./db");
 
+const PORT = process.env.PORT ||5000;
+
 const myServer = http.createServer(app);
 
 const startServer = async() => {
   await connectDB(process.env.MONGODB_URL).then(resp => console.log(resp))
   .catch(err => console.log(err))
 
-  myServer.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}...`);
+  myServer.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
   });
 }
 

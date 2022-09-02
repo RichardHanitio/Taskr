9c6dd3c3843fa1b5885cd6d099160c9184c997c4
Richard Hanitio <richardhan82@gmail.com>
2022-09-02 13:59:25 +0700

    tes

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const path = require("path");
 //     origin: "http://localhost:3000",
 //   })
 // );
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 

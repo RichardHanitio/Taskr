3fb24213beb92f1bc5c9970bb2134e262f2b1e12
Richard Hanitio <richardhan82@gmail.com>
2022-09-02 16:26:31 +0700

    yes

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "/api/v1";
+const API_URL = "https://taskr-tasktracker.herokuapp.com/api/v1";
 
 const httpGetTasks = async () => {
   return await axios.get(`${API_URL}/tasks`);

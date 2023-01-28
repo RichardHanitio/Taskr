2d90db661bd461ae62f6e40d2bebe0fe45a8c8e2
Richard Hanitio <richardhan82@gmail.com>
2023-01-28 10:17:59 +0700

    for production

--- server/app.js
+++ server/app.js
@@ -74,8 +74,8 @@ app.use("/api/v1/tasks", tasksRouter);
 app.use("/auth", authRouter);
 
 // Frontend Router
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 module.exports = app;

958a0728bc800b9b2e05d94044cc09e1601d586b
Richard Hanitio <richardhan82@gmail.com>
2023-01-30 11:17:32 +0700

    serve frontend from server

--- server/app.js
+++ server/app.js
@@ -74,12 +74,12 @@ app.use("/api/v1/tasks", tasksRouter);
 app.use("/auth", authRouter);
 
 // Frontend Router
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Taskr API")
-})
+// app.get("/", (req, res) => {
+//   res.send("Welcome to Taskr API")
+// })
 
 module.exports = app;
